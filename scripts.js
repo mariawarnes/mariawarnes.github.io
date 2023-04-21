86c3a36fa02342eb9bfabeb41d84f708d6603c54
Maria Warnes <mlw05@hotmail.co.uk>
2023-04-21 07:27:44 +0100

    New site in progress

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function activateMessages(i) {
+    const messages = document.querySelectorAll('.message');
+
+    messages[i].className += " active";
+
+    if (i == messages.length -1) {
+        return;
+    }
+
+    setTimeout(function () {
+        activateMessages(i + 1);
+    }, 1500);
+}
+
+activateMessages(0);
